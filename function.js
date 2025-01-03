69d7016f7cf895c9220d0126bf886c78d506b109
hellosiyam <hellosiyambd@gmail.com>
2025-01-03 16:36:46 +0600

    Todays my first code

---
+++ function.js
@@ -0,0 +1,10 @@
+function teethWash() {
+    console.log("Dath Brash kor");
+    console.log("hate beshi somoy nai");
+    console.log("koto khon lagbe!");
+}
+teethWash()
+
+console.log("Today we are going to walkout");
+console.log("-------------");
+teethWash()
