a0fd6e732aab75a82e0c85ae08cf1b510037b4eb
hellosiyam <hellosiyambd@gmail.com>
2025-01-03 18:37:20 +0600

    argument

---
+++ argument.js
@@ -0,0 +1,11 @@
+function diffrent(num1, num2) {
+    const def= num1 - num2;
+    console.log(num1, num2, "The Defrant is", def);
+    
+}
+// const fatherAge = 45;
+// const myAge = 15;
+
+diffrent(fatherAge = 45, myAge = 15)
+console.log("The gap is",);
+diffrent(fatherAge, myAge)
