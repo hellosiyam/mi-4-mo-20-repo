6199084c3ba6c7eb442133b3ab6ca0c382762fa3
hellosiyam <hellosiyambd@gmail.com>
2025-01-03 18:30:39 +0600

    Todays my first code

---
+++ perameter.js
@@ -0,0 +1,14 @@
+function sum(a,b) {
+    console.log(a*b);
+}
+sum(16,2)
+sum(6,6)
+
+function add(a, b, c, d, e) {
+    const sum= a+b+c+d+e;
+    console.log(a,b,c,d,e);
+    console.log(sum);
+    
+}
+add(5,6,8,97, 1)
+add(6,7,8)
